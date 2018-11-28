01cbe5ea9179dd1f41b28b8cca60f1044a9b793f
Marcos Lessa <mlessa@gmail.com>
2018-11-28 17:56:56 +0000

    add footer

--- src/hoc/Layout/layout.js
+++ src/hoc/Layout/layout.js
@@ -2,32 +2,33 @@ import React, { Component } from 'react';
 import './layout.css'
 
 import Header from '../../components/Header/header';
+import Footer from '../../components/Footer/footer';
 
 
 class Layout extends Component {
 
-    state = {
-      showNav:false  
-    }
+	state = {
+		showNav:false  
+	}
 
-    toggleSidenav = (action) =>{
-        this.setState({
-            showNav:action
-        })
-    }
+	toggleSidenav = (action) =>{
+			this.setState({
+					showNav:action
+			})
+	}
  
-    render(){
-        return(
-            <div>
-                <Header
-                    showNav={this.state.showNav}
-                    onHideNav={() => this.toggleSidenav(false)}
-                    onOpenNav={() => this.toggleSidenav(true)}
-                />
-                {this.props.children}
-           
-            </div>
-        )
+	render(){
+		return(
+			<div>
+				<Header
+						showNav={this.state.showNav}
+						onHideNav={() => this.toggleSidenav(false)}
+						onOpenNav={() => this.toggleSidenav(true)}
+				/>
+				{this.props.children}
+				<Footer />
+			</div>
+		)
     }
 
 }
