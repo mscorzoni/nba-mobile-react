62a4559c4c3b833637cb96a5a955f6a2fd97b45e
Marcos Lessa <mlessa@gmail.com>
2018-11-29 19:43:51 +0000

    add video list

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Home from './components/Home/home';
 import Layout from './hoc/Layout/layout';
 
 import NewsArticle from './components/Articles/News/Post/index';
+import VideoArticle from './components/Articles/Videos/Video/index';
 
 class Routes extends Component {
   render() {
@@ -13,7 +14,7 @@ class Routes extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/articles/:id" exact component={NewsArticle} />
-          <Route path="/articles/:id" exact component={NewsArticle} />
+          <Route path="/videos/:id" exact component={VideoArticle} />
         </Switch> 
       </Layout>
     );
