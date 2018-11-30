738ac32b8b7ae4aaf0bf9143efaa3505b92233b5
Marcos Lessa <mlessa@gmail.com>
2018-11-30 20:50:51 +0000

    add sign in page'

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -89,10 +89,43 @@ class SignIn extends Component {
     return error;
   }
 
+
+  submitForm = (event, type) => {
+    event.preventDefault();
+
+    if(type !== null ) {
+      let dataToSubmit = {};
+      let formIsValid = true;
+
+      for(let key in this.state.formdata){
+        dataToSubmit[key] = this.state.formdata[key].value
+      }
+      for(let key in this.state.formdata){
+        formIsValid = this.state.formdata[key].valid && formIsValid;
+      }
+      if(formIsValid){
+        this.setState({
+          loading: true,
+          registerError: ''
+        })
+      }
+    }
+  }
+
+  submitButton = () => (
+    this.state.loading ?
+    'loading...'
+    :
+    <div>
+      <button onClick={(event) => this.submitForm(event, false)}>Register now</button>
+      <button onClick={(event) => this.submitForm(event, true) }>Log in</button>
+    </div>
+  )
+
   render() {
     return (
       <div className={styles.logContainer}>
-        <form>
+        <form onSubmit={(event) => this.submitForm(event, null)}>
           <h2>Register / Log In</h2>
           <FormField 
             id={'email'}
@@ -104,6 +137,9 @@ class SignIn extends Component {
             formdata={this.state.formdata.password}
             change={(element) => this.updateForm(element)}
           />
+
+          {this.submitButton()}
+
         </form>
       </div>
     );
