fa8ee5bf94d25d663cae49583ebb80693c9db7ae
Marcos Lessa <mlessa@gmail.com>
2018-12-02 11:49:59 +0000

    add sidenav options

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home/home';
@@ -10,21 +10,20 @@ import NewsMain from './components/Articles/News/Main/index';
 import VideosMain from './components/Articles/Videos/Main';
 import SignIn from './components/signin/signin';
 
-class Routes extends Component {
-  render() {
-    return (
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/news" exact component={NewsMain} />
-          <Route path="/articles/:id" exact component={NewsArticle} />
-          <Route path="/videos/:id" exact component={VideoArticle} />
-          <Route path="/videos" exact component={VideosMain} />
-          <Route path="/sign-in" exact component={SignIn} />
-        </Switch> 
-      </Layout>
-    );
-  }
-}
+
+const Routes = (props) => {
+  return (
+    <Layout user={props.user}>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/news" exact component={NewsMain} />
+        <Route path="/articles/:id" exact component={NewsArticle} />
+        <Route path="/videos/:id" exact component={VideoArticle} />
+        <Route path="/videos" exact component={VideosMain} />
+        <Route path="/sign-in" exact component={SignIn} />
+      </Switch> 
+    </Layout>    
+  );
+};
 
 export default Routes;
