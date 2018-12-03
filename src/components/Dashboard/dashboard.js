038a2180ce1ac1b3142886762fe66b03a7e09f26
Marcos Lessa <mlessa@gmail.com>
2018-12-03 14:47:31 +0000

    add file uploader

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -7,6 +7,8 @@ import { Editor } from 'react-draft-wysiwyg';
 import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
 import { stateToHTML } from 'draft-js-export-html';
 
+import Uploader from '../widgets/FileUploader/fileUploader';
+
 class Dashboard extends Component {
 
 	state = {
@@ -190,11 +192,19 @@ class Dashboard extends Component {
 		})
 	}
 
+	storeFilename = (filename) => {
+		this.updateForm({ id: 'image' }, filename)
+	}
+
   render() {
     return (
       <div className={styles.postContainer}>
         <form onSubmit={this.submitForm}>
           <h2>Add Post</h2>
+
+					<Uploader
+						filename={(filename) => this.storeFilename(filename)}
+					/>
 			
 					<FormField
 						id={'author'}
