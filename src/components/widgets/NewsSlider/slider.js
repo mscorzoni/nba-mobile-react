0ba6c339cbc7dce6ba9784d8496532a09e52b330
Marcos Lessa <mlessa@gmail.com>
2018-12-03 21:03:49 +0000

    fix bug slider

--- src/components/widgets/NewsSlider/slider.js
+++ src/components/widgets/NewsSlider/slider.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { firebaseArticles, firebaseLooper } from '../../../firebase';
+import { firebase, firebaseArticles, firebaseLooper } from '../../../firebase';
 
 import SliderTemplates from './slider_templates';
 
@@ -13,11 +13,29 @@ class NewsSlider extends Component {
   componentWillMount(){
 		firebaseArticles.limitToFirst(3).once('value')
 		.then((snapshot) => {
-			const news = firebaseLooper(snapshot)
-			this.setState({
-				news
-			})
-		})
+      const news = firebaseLooper(snapshot)
+      
+       
+      const asyncFunction = (item, i, cb) => {
+         firebase.storage().ref('images')
+        .child(item.image).getDownloadURL()
+        .then(url => {
+          news[i].image = url;
+          cb();
+        })
+      }
+
+      let requests = news.map((item, i) => {
+        return new Promise((resolve) => {
+          asyncFunction(item, i, resolve)
+        })
+      })
+      Promise.all(requests).then(() =>{
+        this.setState({
+          news
+        })
+      })
+		});
 
     // axios.get(`${URL}/articles?_start=${this.props.start}&_end=${this.props.amount}`)
     //   .then( response => {
